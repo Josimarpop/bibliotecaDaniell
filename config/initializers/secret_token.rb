06dc63e2ae080a61b525f017c90db9fa7fe44b7f
Daniell Wagner Azevedo de Lima <daniell@mixincode.com>
2013-04-18 22:23:53 -0300

    Criacao do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BibliotecaDaniell::Application.config.secret_token = '31ba7dfb4fc889e4a04575308dbcff3c5400a215944d88dc90672b1675f003f0f1521224369838651f88c86f4356ca66519d0d61a6e2db7fb786f55a0334d7f8'
